c8de956fedda256e6cd28fd92654f406ce2b3d80
Pavel Vorobev <p.vorobyev@inbox.ru>
2021-05-06 15:06:23 +0300

    webpack with development web-server

---
+++ ch6/project_dev/webpack.config.js
@@ -0,0 +1,14 @@
+const {resolve} = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'index.bundle.js',
+        path: resolve(__dirname, 'dist')
+    },
+    target: 'node',
+    mode: 'development',
+    devServer: {
+        contentBase: '.'
+    }
+}
